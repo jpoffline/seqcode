a355772b5fbe2c8a64323bba26557af593aaddc2
jpUoN <j.pearson@nottingham.ac.uk>
2015-01-14 12:57:00 +0000

    suff

--- pyplot/plottools.py
+++ pyplot/plottools.py
@@ -2,39 +2,45 @@ from pylab import *
 import pylab as P
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
-
+from scipy import stats
 
 # Function to read in data (possibly with comments, marked on with '#'
 # then return the required columns
 def data_column(df, (p1, p2), fac):
-	d = open(df,'r');
-	lines = d.readlines();
-	d.close();
-	x = []
-	y = []
-	for line in lines:
-		if line.startswith('#'):
-			continue
-		else:
-			p = line.split()
-			x.append(float(p[p1]) * fac[0])
-			y.append(float(p[p2]) * fac[1])
-
-	return (np.array(x), np.array(y))
+    d = open(df,'r');
+    lines = d.readlines();
+    d.close();
+    x = []
+    y = []
+    line_num = 0
+    line_BURNS = 1000
+    for line in lines:
+    	if line.startswith('#'):
+    		continue
+    	else:
+            if line_num > line_BURNS:
+                p = line.split()
+                x.append(float(p[p1]) * fac[0])
+                y.append(float(p[p2]) * fac[1])
+        line_num+=1
+    return (np.array(x), np.array(y))
     
 def data_1d(df, p1, fac):
-	d = open(df,'r');
-	lines = d.readlines();
-	d.close();
-	x = []
-	for line in lines:
-		if line.startswith('#'):
-			continue
-		else:
-			p = line.split()
-			x.append(float(p[p1]) * fac)
-
-	return np.array(x)    	
+    d = open(df,'r');
+    lines = d.readlines();
+    d.close();
+    x = []
+    line_num = 0
+    line_BURNS = 1000
+    for line in lines:
+    	if line.startswith('#'):
+    		continue
+    	else:
+            if line_num > line_BURNS:
+                p = line.split()
+                x.append(float(p[p1]) * fac)
+        line_num+=1        
+    return np.array(x)    	
 
 # Function to plot data with latex labels
 def plot( (X,Y), label, output_fig_name, fig_fontsize):
@@ -48,7 +54,7 @@ def plot( (X,Y), label, output_fig_name, fig_fontsize):
     pp.close()
 
 def plot_1dhist(data, nbins, axes_labels,out_fig_filename, fig_fontsize):
-    pp = PdfPages(out_fig_filename,fig_fontsize)
+    pp = PdfPages(out_fig_filename)
     plt.figure()
     plt.clf()
     n, bins, patches = plt.hist(data, nbins, normed=1, histtype='step')
@@ -61,9 +67,13 @@ def plot_2dhist(data_plottable, lab, nbins, fig_fontsize, output_fig_name):
     pp = PdfPages(output_fig_name)
     plt.figure()
     plt.clf()
-    plt.hist2d(data_plottable[0], data_plottable[1], bins = nbins)
+    h,d,dd,ddd = plt.hist2d(data_plottable[0], data_plottable[1], bins = nbins)
     plt.xlabel(lab[0], fontsize = fig_fontsize)
     plt.ylabel(lab[1], fontsize = fig_fontsize)
+#    plt.clf()
+#    plt.imshow(h, origin = "lower", interpolation = "gaussian")
+    kernel = stats.gaussian_kde(data_plottable)
+    Z = np.reshape(kernel(positions).T, X.shape)
     plt.colorbar()
     pp.savefig()
     pp.close() 
